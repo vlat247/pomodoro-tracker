68c929f773d509de7378d657d0cf2e3cde805693
vlat <anonim270208@gmail.com>
2025-05-20 13:54:52 +0500

    fixed the inventory button

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "firebase/firestore";    
     import { 
       getAuth, 
       createUserWithEmailAndPassword,
